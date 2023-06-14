e698bc83bb7886876dd0a89c48b51a79871d9624
eavenreager <narutoboruto26@gmail.com>
2023-06-14 07:45:56 +0700

    updateBarangMasukActivity

--- app/src/main/java/com/example/slavaamorial/BarangMasukActivity.java
+++ app/src/main/java/com/example/slavaamorial/BarangMasukActivity.java
@@ -2,13 +2,29 @@ package com.example.slavaamorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BarangMasukActivity extends AppCompatActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barang_masuk);
+
+        backButton=(Button) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(BarangMasukActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
